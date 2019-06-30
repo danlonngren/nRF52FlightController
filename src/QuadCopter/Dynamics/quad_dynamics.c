bc1c8ad1304e39f97d4d7b26d7bb06c6468363a0
danlonngren <dan@molecularwarehouse.com>
2019-07-01 00:47:57 +0100

    Refactored and updated new orientation, y = pitch, x = roll and z = yaw.

--- src/QuadCopter/Dynamics/quad_dynamics.c
+++ src/QuadCopter/Dynamics/quad_dynamics.c
@@ -7,13 +7,13 @@
 
 
 #include "quad_dynamics.h"
-#include <math.h>
 
 #include "filters.h"
 
-#ifndef PI
-#define PI  3.14159
-#endif
+#include "nrf_port.h"
+
+#include "common_math.h"
+
 
 /* 
 @TODO:
@@ -24,68 +24,42 @@
 
 */
 
-float filterAngle;
-float dt=0.02;
 
-float comp_filter(float newAngle, float newRate) {
-
-    float filterTerm0;
-    float filterTerm1;
-    float filterTerm2;
-    float timeConstant;
-    
-    timeConstant=0.5; // default 1.0
-    
-    filterTerm0 = (newAngle - filterAngle) * timeConstant * timeConstant;
-    filterTerm2 += filterTerm0 * dt;
-    filterTerm1 = filterTerm2 + ((newAngle - filterAngle) * 2 * timeConstant) + newRate;
-    filterAngle = (filterTerm1 * dt) + filterAngle;
-    float previousAngle = filterAngle;
-    return previousAngle; // This is actually the current angle, but is stored for the next iteration
-}
-
-
-void qdCalcAccAngles(float *aX, float *aY, float x, float y, float z)
-{
-    uint32_t acc_vector = sqrt((x * x) + (y * y) + (z * z));
-    *aX = asin((float)y / (float)acc_vector) * 57.296;
-    *aY = asin((float)x / (float)acc_vector) * -57.296;
-}
-#include "nrf_port.h"
-void qdCalculateAngleFromAcc(float *pitch, float *roll, int16_t ax, int16_t ay, int16_t az)
+/**@breif Calculate angles from accelerometer */
+void qdCalculateAngleFromAcc(float *pitch, float *roll, float ax, float ay, float az)
 {
     float accPitch = 0, accRoll = 0;
 
     float acc_vector = sqrtf(((float)ax * ax) + ((float)ay * ay) + ((float)az * az));
     if (fabs(ay) < acc_vector)
     {
-    	accPitch = asin((float)ay / (float)acc_vector) * 57.296;
+    	accRoll = asin((float)ay / (float)acc_vector) * 57.296;
     }
     if (fabs(ax) < acc_vector)
     {	
-        accRoll = asin((float)ax / (float)acc_vector) * -57.296;
+        accPitch = asin((float)ax / (float)acc_vector) * -57.296;
     }
     *pitch = accPitch;
     *roll  = accRoll;
 }
 
-void qdCalculateAttitude(attitude_t *cAttitude, int16_t gx, int16_t gy, int16_t gz, int16_t ax, int16_t ay, int16_t az)
+void qdCalculateAngle(attitude_t *cAttitude, float gyroRate[], float acc[])
 {
-    float constant1 = cAttitude->dt / 1000.0; // Convert into seconds
-    cAttitude->pitch += (float)gx * constant1;
-    cAttitude->roll  += (float)gy * constant1;
-    cAttitude->yaw = gz ; // += (float)gz * constant1;
+    float constant1 = cAttitude->dt; // Convert into seconds
+    cAttitude->roll += gyroRate[0] * constant1;
+    cAttitude->pitch  += gyroRate[1] * constant1;
+    cAttitude->yaw = gyroRate[2]; // += (float)gz * constant1;
     
-    float constant2 = sin((float)gz * (constant1 * 0.017453277)); // (PI / 180.0)
+    float constant2 = sin(gyroRate[2] * (constant1 * 0.017453277)); // (PI / 180.0)
     cAttitude->pitch += cAttitude->roll * constant2;
     cAttitude->roll  -= cAttitude->pitch * constant2;
     
     float accPitch = 0, accRoll = 0;
-    qdCalculateAngleFromAcc(&accPitch, &accRoll, ax, ay, az);
+    qdCalculateAngleFromAcc(&accPitch, &accRoll, acc[0], acc[1], acc[2]);
 
     // Fuse acc And Gyro data
-    cAttitude->pitch = filterComplementrary(cAttitude->pitch, accPitch, 0.9996);
-    cAttitude->roll  = filterComplementrary(cAttitude->roll, accRoll, 0.9996);
+    cAttitude->pitch = FILTER_COMP(cAttitude->pitch, accPitch, 0.9998);
+    cAttitude->roll  = FILTER_COMP(cAttitude->roll, accRoll,   0.9998);
 }
 
 
@@ -102,7 +76,7 @@ void qdCalculateSetPoints(attitude_t *att, float rcPitch, float rcRoll, float rc
     }
 
     // Get receivers and calculate setPoints for PID.
-    att->pitchSet  = (1500.0f - rcPitch);
+    att->pitchSet  = (rcPitch - 1500.0f);
     att->pitchSet -= pitchAdjust;
     att->pitchSet /= setPointDiv;
 
--- src/QuadCopter/Dynamics/quad_dynamics.h
+++ src/QuadCopter/Dynamics/quad_dynamics.h
@@ -34,10 +34,10 @@ typedef struct
 void qdCalculateAttitudeTest(attitude_t *outAttitude, attitude_t *gyro, attitude_t *acc, uint32_t dt);
 
 
-void qdCalculateAngleFromAcc(float *pitch, float *roll, int16_t ax, int16_t ay, int16_t az);
+void qdCalculateAngleFromAcc(float *pitch, float *roll, float ax, float ay, float az);
 
 
-void qdCalculateAttitude(attitude_t *cAttitude, int16_t gx, int16_t gy, int16_t gz, int16_t ax, int16_t ay, int16_t az);
+void qdCalculateAngle(attitude_t *cAttitude, float gyroRate[], float acc[]);
 
 
 void qdCalculateSetPoints(attitude_t *att, float rcPitch, float rcRoll, float rcYaw, float adjustGain, float setPointDiv, bool autoLevel);
