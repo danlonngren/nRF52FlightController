e279db6c86dc25e3f38fce5303e3cae9a3ce5074
danlonngren <dan@molecularwarehouse.com>
2019-07-01 23:11:29 +0100

    Added new quick acos/atan2 functions and

---
+++ src/QuadCopter/Dynamics/common_math.c
@@ -0,0 +1,44 @@
+#include "common_math.h"
+
+// https://github.com/Crashpilot1000/HarakiriWebstore1/blob/396715f73c6fcf859e0db0f34e12fe44bace6483/src/mw.c#L1292
+// http://http.developer.nvidia.com/Cg/atan2.html (not working correctly!)
+// Poly coefficients by @ledvinap (https://github.com/cleanflight/cleanflight/pull/1107)
+// Max absolute error 0,000027 degree
+float atan2_approx(float y, float x)
+{
+    #define atanPolyCoef1  3.14551665884836e-07f
+    #define atanPolyCoef2  0.99997356613987f
+    #define atanPolyCoef3  0.14744007058297684f
+    #define atanPolyCoef4  0.3099814292351353f
+    #define atanPolyCoef5  0.05030176425872175f
+    #define atanPolyCoef6  0.1471039133652469f
+    #define atanPolyCoef7  0.6444640676891548f
+
+    float res, absX, absY;
+    absX = fabsf(x);
+    absY = fabsf(y);
+
+    res  = MAX(absX, absY);
+
+    if (res) res = MIN(absX, absY) / res;
+    else res = 0.0f;
+    res = -((((atanPolyCoef5 * res - atanPolyCoef4) * res - atanPolyCoef3) * res - atanPolyCoef2) * res - atanPolyCoef1) / ((atanPolyCoef7 * res + atanPolyCoef6) * res + 1.0f);
+    if (absY > absX) res = (PI / 2.0f) - res;
+    if (x < 0) res = PI - res;
+    if (y < 0) res = -res;
+    return res;
+}
+
+// http://http.developer.nvidia.com/Cg/acos.html
+// Handbook of Mathematical Functions
+// M. Abramowitz and I.A. Stegun, Ed.
+// Absolute error <= 6.7e-5
+float acos_approx(float x)
+{
+    float xa = fabsf(x);
+    float result = sqrtf(1.0f - xa) * (1.5707288f + xa * (-0.2121144f + xa * (0.0742610f + (-0.0187293f * xa))));
+    if (x < 0.0f)
+        return PI - result;
+    else
+        return result;
+}
--- src/QuadCopter/Dynamics/common_math.h
+++ src/QuadCopter/Dynamics/common_math.h
@@ -2,14 +2,28 @@
 #define COMMON_MATH_H
 
 #include <stdint.h>
+#include <stdbool.h>
+
+#include "nordic_common.h"
+#ifndef MIN
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#endif
+#ifndef MAX
+#define MAX(a,b) (((a)>(b))?(a):(b))
+#endif
 
 #define g 9.81f // 1g ~ 9.81 m/s^2
 
 #ifndef PI
-#define PI  3.14159
+#define PI  3.14159265358979323846f
 #endif
 
 #include <math.h>
+
+float acos_approx(float x);
+
+float atan2_approx(float y, float x);
+
 
 static inline float toRadians(float degrees)
 {
@@ -24,15 +38,21 @@ static inline float toDegrees(float radians)
 
 static inline float invSqrt(float x) 
 {
-	float halfx = 0.5f * x;
-	float y = x;
-	long i = *(long*)&y;
-	i = 0x5f3759df - (i>>1);
-	y = *(float*)&i;
-	y = y * (1.5f - (halfx * y * y));
-	return y;
+    float halfx = 0.5f * x;
+    float y = x;
+    long i = *(long*)&y;
+    i = 0x5f3759df - (i>>1);
+    y = *(float*)&i;
+    y = y * (1.5f - (halfx * y * y));
+    return y;
 }
 
+static inline float mathConstrain(float target, float min, float max)
+{
+    if (target > max) target = max;
+    else if (target < min) target = min;
+    return target;
+}
 
 #define US_TO_SECONDS(x) (x / 1000000.0f)
 
